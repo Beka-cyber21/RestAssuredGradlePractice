b288a02d7ba71d32af467b4981566d92ff4a7743
BekaAkmatov <bekturakmatov21@gmail.com>
2025-12-07 02:17:39 +0600

    First Test Start (But It Wasn't static(I need to fix it))

---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class TextBoxTest extends BaseTest{
+    @Test
+    public void firstTest(){
+        driver.get("https://demoqa.com/text-box");
+        textBoxPage.createNewAccountWithConstant();
+
+    }
+}
