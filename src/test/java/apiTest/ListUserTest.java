e928e8a0f8b4ea4cf0905e3c1ebf6be20ca2d749
BekaAkmatov <bekturakmatov21@gmail.com>
2025-12-09 14:09:21 +0600

    Create New Api Test

--- src/test/java/apiTest/ListUserTest.java
+++ src/test/java/apiTest/ListUserTest.java
@@ -1,4 +1,43 @@
 package apiTest;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class ListUserTest {
+    @Test
+    void testHttpGet() {
+        given()
+                .contentType("application/json")
+                .when()
+                .get("https://httpbin.org/get")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    void testHttpPost() {
+        given()
+                .contentType("application/json")
+                .body("{\"name\": \"Johnn\", \"age\": 30}")
+                .when()
+                .patch("https://httpbin.org/patch n")
+                .then()
+                .statusCode(200)
+                .body("json.name", equalTo("John"));
+    }
+    @Test
+    public void userListTest() {
+        given()
+                .log().all() // ← Логируем ЗАПРОС
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users") // ← Используем httpbin
+                .then()
+                .log().all() // ← Логируем ОТВЕТ
+                .statusCode(200);
+    }
 }
+
